aa85699098f67577e4b1faca6d2542e78ff01908
krishnai18 <82368339+krishnai18@users.noreply.github.com>
2021-07-08 14:29:46 +0530

    Add files via upload

---
+++ Blazorcrud/Pages/AddEmployee.razor.cs
@@ -0,0 +1,18 @@
+﻿using Blazorcrud.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazorcrud.Pages
+{
+    public partial class AddEmployee
+    {
+        [Inject] public ApiService service { get; set; }
+        protected async void PostEmployee()
+        {
+            service.CreateEmployee(obj);
+        }
+    }
+}
